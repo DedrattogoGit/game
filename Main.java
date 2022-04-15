99b7bf6f271d9d3cd75547e73bbf072bdcf13ad1
DedrattogoGit <103758654+DedrattogoGit@users.noreply.github.com>
2022-04-15 09:24:46 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,63 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Calculator.\nThis is where you use a machine to calculate.");
+		  Scanner hacker = new Scanner(System.in);
+		System.out.println("Choose a number.");
+		int a = hacker.nextInt();
+		  Scanner hacke = new Scanner(System.in);
+		System.out.println("Choose another number.");
+		int b = hacke.nextInt();
+		Scanner op = new Scanner(System.in);
+		System.out.println("Choose an operation. 1 for X, 2 for /, 3 for +, 4 for -, and 5 for %.");
+		int c = op.nextInt();
+		if (c == 1) {
+		    System.out.println(a);
+            System.out.println("x");
+            System.out.println(b);
+            System.out.println("is");
+            System.out.println(a * b);
+		}
+		if (c == 2) {
+		    System.out.println(a);
+            System.out.println("/");
+            System.out.println(b);
+            System.out.println("is");
+            System.out.println(a / b);
+		}
+		if (c == 3) {
+		    System.out.println(a);
+            System.out.println("+");
+            System.out.println(b);
+            System.out.println("is");
+            System.out.println(a + b);
+		}
+		if (c == 4) {
+		    System.out.println(a);
+            System.out.println("-");
+            System.out.println(b);
+            System.out.println("is");
+            System.out.println(a - b);
+		}
+		if (c == 5) {
+		    System.out.println(a);
+            System.out.println("%");
+            System.out.println(b);
+            System.out.println("is");
+            System.out.println(a % b);
+		}
+	}
+}
+
